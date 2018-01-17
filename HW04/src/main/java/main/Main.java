9ce2e6243fd645f0a0769a5ab3f7d93281016544
Alexander <flysoohigh63@gmail.com>
2018-01-17 15:00:58 +0400

    HW04

--- HW04/src/main/java/main/Main.java
+++ HW04/src/main/java/main/Main.java
@@ -8,13 +8,6 @@ import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
-/**
- * @author v.chibrikov
- *         <p/>
- *         Пример кода для курса на https://stepic.org/
- *         <p/>
- *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
- */
 public class Main {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
@@ -24,7 +17,7 @@ public class Main {
 
         ResourceHandler resource_handler = new ResourceHandler();
         resource_handler.setDirectoriesListed(true);
-        resource_handler.setResourceBase("L4.1 WebSockets/public_html");
+//        resource_handler.setResourceBase("L4.1 WebSockets/public_html");
 
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[]{resource_handler, context});
