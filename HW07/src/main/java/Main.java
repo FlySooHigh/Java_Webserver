6cd3fa42df7a8448b80abe123412f92cc014e11c
Alexander <flysoohigh63@gmail.com>
2018-02-09 17:52:26 +0400

    HW07

---
+++ HW07/src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * Created by sbt-kadatskiy-av on 09.02.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        boolean listening = true;
+        try (ServerSocket serverSocket = new ServerSocket(5050);) {
+            System.out.println("Server started");
+            while(listening){
+                 new SocketThread(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ HW07/src/main/java/SocketThread.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by sbt-kadatskiy-av on 09.02.2018.
+ */
+public class SocketThread extends Thread{
+    private Socket clientSocket;
+
+    public SocketThread(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run(){
+        try(PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                out.println(inputLine);
+                if (inputLine.equals("Bue.")) break;
+            }
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
