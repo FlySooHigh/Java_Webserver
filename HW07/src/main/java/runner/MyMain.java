4c4ee2c38f13a22124fb251a68eb785bb82ae5af
Alexander <flysoohigh63@gmail.com>
2018-02-09 18:18:42 +0400

    HW07 changed package

--- HW07/src/main/java/runner/MyMain.java
+++ HW07/src/main/java/runner/MyMain.java
@@ -1,16 +1,18 @@
+package runner;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 
 /**
  * Created by sbt-kadatskiy-av on 09.02.2018.
  */
-public class Main {
+public class MyMain {
     public static void main(String[] args) {
         boolean listening = true;
         try (ServerSocket serverSocket = new ServerSocket(5050);) {
             System.out.println("Server started");
             while(listening){
-                 new SocketThread(serverSocket.accept()).start();
+                new SocketThread(serverSocket.accept()).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
--- HW07/src/main/java/runner/SocketThread.java
+++ HW07/src/main/java/runner/SocketThread.java
@@ -1,3 +1,5 @@
+package runner;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
